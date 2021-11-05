54afec957cf70caf94b997dce7b8d80e9862a64f
fresh-avocado <gabriel.spranger@utec.edu.pe>
2021-11-05 10:11:13 -0500

    added .env, userService to encapsulate logic, created logger service

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,23 +1,27 @@
 import express, { Request, Response, Router } from "express";
-import logger from "../services/logger";
 
-import User from "../models/User";
 import { USER } from "../utils/constants/messages";
+import { UserDTO } from "../utils/dtos/user";
+
+import logger from "../services/logger";
+import userService from "../services/userService";
 
 const userController: Router = express.Router();
 
 userController.post("/user", (req: Request, res: Response) => {
-  logger.info("user creation body", req.body);
-
-  // TODO: mover esta lógica al service
-  const newUser = User.create(req.body, (err: any, result: any) => {
-    if (err) {
+  // TODO: validación de `req.body`
+  userService
+    .create(req.body)
+    .then((newUser: UserDTO) => {
+      logger.info("user creation", newUser);
+      return res
+        .status(201)
+        .json({ msg: USER.SUCESS.CREATION, newUser: newUser });
+    })
+    .catch((err) => {
       logger.error("user creation", err);
       return res.status(500).json({ msg: `${USER.ERROR.CREATION}.\n${err}` });
-    }
-    logger.info("user creation body", newUser);
-    return res.status(201).json({ msg: USER.SUCESS.CREATION, newUser: result });
-  });
+    });
 });
 
 export default userController;
---
+++ src/utils/dtos/user.ts
@@ -0,0 +1,28 @@
+interface UserChat {
+  chatId: string;
+  key: Buffer;
+}
+
+interface OutgoingRequest {
+  a: Buffer;
+  p: Buffer;
+  g: Buffer;
+  userId: string;
+}
+
+interface IncomingRequest {
+  peerPrivatePart: Buffer;
+  p: Buffer;
+  g: Buffer;
+  userId: string;
+}
+
+export interface UserDTO {
+  username: string;
+  password: string;
+  email: string;
+  verified: boolean;
+  chats: UserChat[];
+  outgoingRequests: OutgoingRequest[];
+  incomingRequests: IncomingRequest[];
+}
