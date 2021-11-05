3eaf5631c64a2982b1361cd8e8ac044d108f7bf6
fresh-avocado <gabriel.spranger@utec.edu.pe>
2021-11-05 06:46:32 -0500

    chat secret key better to be buffer than string

--- src/models/User.ts
+++ src/models/User.ts
@@ -13,7 +13,7 @@ const UserSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "Chat",
       },
-      key: String,
+      key: Schema.Types.Buffer,
     },
   ],
   outgoingRequests: [
