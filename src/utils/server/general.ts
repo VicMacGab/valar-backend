073981602b82b3e2bb6a88bbc9132d787cd38c1f
fresh-avocado <gabriel.spranger@utec.edu.pe>
2021-12-04 15:16:04 -0500

    updated deploy.yml to deploy to our server, updated TLS key paths

--- src/utils/server/general.ts
+++ src/utils/server/general.ts
@@ -9,9 +9,12 @@ export const getServerFrom = (
   api: express.Express
 ): http.Server | https.Server => {
   if (process.env.NODE_ENV == "production") {
-    const certAndKeyPath = path.join(__dirname, "../../../tls");
-    const privateKey = fs.readFileSync(`${certAndKeyPath}/server.key`, "utf8");
-    const certificate = fs.readFileSync(`${certAndKeyPath}/server.crt`, "utf8");
+    const certAndKeyPath = "/etc/letsencrypt/live/api.cliffdev.com";
+    const privateKey = fs.readFileSync(`${certAndKeyPath}/privkey.pem`, "utf8");
+    const certificate = fs.readFileSync(
+      `${certAndKeyPath}/fullchain.pem`,
+      "utf8"
+    );
     const credentials = { key: privateKey, cert: certificate };
     const httpsServer = https.createServer(credentials, api);
     logger.info("sucessfully created https server");
